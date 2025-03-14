da1169039d1ac5009e08b870aeac36702bdd109d
rikuto-g-1 <rikuphonemen@gmail.com>
2025-03-14 14:16:09 +0900

    post_testのコメントアウト

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,7 @@
-#require "test_helper"
+require "test_helper"
 
-#class PostTest < ActiveSupport::TestCase
+class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-#end
+end
