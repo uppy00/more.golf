7d23ed32ee444767c101caf40731264dce2f9139
rikuto-g-1 <rikuphonemen@gmail.com>
2025-03-17 15:56:04 +0900

    logout,in,user.newのフラッシュメッセージは完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "ユーザーが作成されました"
+      flash[:success] = 'ユーザーの登録に成功しました'
       redirect_to root_path
     else
-      flash.now[:alert] = "ユーザーの作成に失敗しました。エラーを確認してください"
+      flash.now[:danger] = "ユーザーの作成に失敗しました。エラーを確認してください"
       render :new
     end
   end
